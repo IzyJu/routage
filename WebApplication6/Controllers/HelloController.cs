e3065cf1ebbaecc0d984f248dee85329cf7a2a72
Julien.Iz <69205008+IzyJu@users.noreply.github.com>
2020-11-23 10:49:11 +0100

    Add files via upload

---
+++ WebApplication6/Controllers/HelloController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication6.Controllers
+{
+    [Route("/{action}")]
+    public class HelloController
+    {
+        [Route("/Say")]
+        public String SayHello()
+        {
+            return "Hello !";
+        }
+
+        [Route("/Yell")]
+        public String YellHello()
+        {
+            return "HELLO !";
+        }
+    }
+}
